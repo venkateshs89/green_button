72b53e76213b30a8087e844a85c6b02a61f69164
Venkatesh Sanipini <venkatesh@progton.com>
2021-04-11 08:58:54 +0530

    auto_complete endpoint changes

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
 
   before_action :set_product, only: [:show, :product_detail]
+  before_action :set_products, only: [:auto_complete]
 
   def show
     render json: { data: ProductSerializer.new(@product).to_h }, status: 200
@@ -13,7 +14,7 @@ class Api::V1::ProductsController < ApplicationController
   def auto_complete
     order_by = Arel.sql('LENGTH(name) ASC')
     query = params[:term].to_s
-    sanitized_data = Product.where('lower(name) ILIKE :query', query: "%#{query}%").order(order_by).limit(30)
+    sanitized_data = @products.where('lower(name) ILIKE :query', query: "%#{query}%").order(order_by).limit(30)
     results = sanitized_data.map { |x| [x.id, x.name, x.slug] }
     render json: results.map { |x| { id: x[0], full_name: x[1], slug: x[2] } }, status: 200
   end
@@ -24,5 +25,22 @@ class Api::V1::ProductsController < ApplicationController
     @product =  Product.friendly.find(params[:id])
   rescue
     render json: { message: 'Product not found' }, status: 404
+  end
+
+  def set_products
+    case params[:entity_type]
+    when 'storefront'
+      storefront = Storefront.friendly.find(params[:entity_slug])
+      @products = storefront.products
+    when 'brand'
+      brand = Brand.friendly.find(params[:entity_slug])
+      @products = brand.products
+    when 'product'
+      product = Product.friendly.find(params[:entity_slug])
+      storefront = product.resource
+      @products = storefront.products
+    else
+      @products = Product.all
+    end
   end
 end
